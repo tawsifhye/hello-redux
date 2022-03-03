5d1f5972349bfccf1a3b58f617d15ccf049885c1
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-03-03 18:13:30 +0600

    extention helper added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { dataReducer } from "./reducers/dataReducer";
 
 
 const reducers = combineReducers({ dataReducer })
 
-export const store = createStore(reducers, applyMiddleware(thunk));
+export const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
